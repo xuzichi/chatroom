8ead08718ef1b9db0610eb1404e5017af1f58caf
CQY <1664885075@qq.com>
2019-11-10 16:47:14 +0800

    最终版本

---
+++ chatbot.py
@@ -0,0 +1,20 @@
+from chatterbot import ChatBot
+
+bot = ChatBot(
+    'my-chat',
+    database_uri='sqlite:///db.sqlite3'
+)
+
+print('Type something to begin...')
+
+while True:
+    try:
+        user_input = input()
+
+        bot_response = bot.get_response(user_input)
+
+        print(bot_response)
+
+    # Press ctrl-c or ctrl-d on the keyboard to exit
+    except (KeyboardInterrupt, EOFError, SystemExit):
+        break
